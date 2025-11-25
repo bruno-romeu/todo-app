21f8808581946bf81162f59381a7325dd2b97705
Bruno Romeu da Silva <bruno.rsilva2004@gmail.com>
2025-11-24 23:08:14 -0300

    implementação da mudança de estado no app no momento de pesquisa e de marcação do checkbox (marcar tarefa como concluída).

--- lib/main.dart
+++ lib/main.dart
@@ -6,13 +6,64 @@ void main() {
 }
 
 class ListTarefasApp extends StatelessWidget {
+  const ListTarefasApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(home: ListaScreen());
   }
 }
 
-class ListaScreen extends StatelessWidget {
+class ListaScreen extends StatefulWidget {
+
+  @override
+  State<StatefulWidget> createState() {
+    return ListaScreenState();
+  }
+}
+
+class ListaScreenState extends State<ListaScreen> {
+  List<Tarefa> tarefas = [];
+  TextEditingController controller = TextEditingController();
+
+  void adicionarTarefa(String nome) {
+    setState(() {
+      tarefas.add(Tarefa(nome));
+      controller.clear();
+    });
+  }
+
+  Widget getItem(Tarefa tarefa) {
+
+
+    return Row(
+      children: [
+        IconButton (
+          icon: Icon(tarefa.concluida ? 
+          Icons.check_box : 
+          Icons.check_box_outline_blank,
+          color: Colors.green,
+          size: 42
+          ),
+          iconSize: 42,
+          onPressed: () {
+            setState(() {
+              tarefa.concluida = !tarefa.concluida;
+            });
+          },),
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(tarefa.nome), 
+            Text(
+              '${tarefa.data.day.toString()}/${tarefa.data.month.toString()}/${tarefa.data.year.toString()}'
+            )
+          ],
+        ),
+      ],
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -23,33 +74,31 @@ class ListaScreen extends StatelessWidget {
       ),
 
       body: Column(
-        children: [
-          TextField(
-            decoration: InputDecoration(
-              label: Text('Digite uma nova tarefa'),
-              border: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(8),
-                borderSide: BorderSide(
-                  color: Colors.blueAccent,
-                  style: BorderStyle.solid,
-                ),
+        
+        mainAxisAlignment: MainAxisAlignment.start,
+        mainAxisSize: MainAxisSize.max,
+        children: <Widget>[
+          Container(
+            padding: EdgeInsets.all(8),
+            child: TextField(
+              decoration: InputDecoration(
+                label: Text('Digite uma nova tarefa...'),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(8)
+                )
               ),
+              controller: controller,
+              onSubmitted: adicionarTarefa
             ),
           ),
-          ListView(
-            shrinkWrap: true,
-            children: [
-              Row(
-                children: [
-                  Checkbox(value: false, onChanged: (bool? value) {}),
-                  Column(
-                    children: [Text('Lavar o Carro'), Text('24/11/2025 17:35')],
-                  ),
-                ],
-              ),
-            ],
-          ),
-        ],
+          Expanded(
+            child: ListView.builder(
+              itemCount: tarefas.length,
+              itemBuilder: (_, indice) {
+                return getItem(tarefas[indice]);
+              }
+          )),
+        ]
       ),
     );
   }
